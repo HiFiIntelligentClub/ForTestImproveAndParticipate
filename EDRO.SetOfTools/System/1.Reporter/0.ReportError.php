7fa111380e0ea70c843f77f1e8e42d4226d8510a
Andrey Chekmarev <assminog@gmail.com>
2021-03-20 05:00:36 +0300

    Add files via upload

---
+++ EDRO.SetOfTools/System/1.Reporter/0.ReportError.php
@@ -0,0 +1,32 @@
+<?php
+/*© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru*/
+////// 
+   //   /\ RCe
+  //  <  **> 
+ //     Jl   
+//////
+echo '[v]0.ReportError.php'."\n";
+class ReportError
+	{
+	public $html;
+	public function __construct($_objKIIM, $_strMessage)
+		{
+		/*echo '<warning
+			class="block"
+			style="color:red;"
+			>';
+			echo $_strMessage;
+		echo '</warning>';*/
+		}
+	private function _ShowMessage($_strMessage)
+		{
+		//print_r($this);
+		//echo '<Error class="block" style="color:red;">'.$_strMessage."</Error>";
+		}
+	public static function _Admin($_objKIIM, $_strMessage)
+		{
+		}
+	}
+echo '[.]0.ReportError.php'."\n";
+$ч0ВыполненоЧастей++;
+?>
